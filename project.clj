0b8e4e3dc426bbc6fae99514d8a1ddf95927bee8
Alexey Popov <a.popov@zima.io>
2018-06-22 13:33:30 +0300

    deps update

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject beanstalk-clj "0.1.3"
+(defproject alexeypopov/beanstalk-clj "0.1.4-SNAPSHOT"
   :description "A simple beanstalkd client library for Clojure."
-  :url "http://github.com/soasme/beanstalk-clj"
+  :url "http://github.com/AlexeyPopov/beanstalk-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [clj-yaml "0.4.0"]
-                 [slingshot "0.10.3"]]
+                 [slingshot "0.12.2"]]
   :repl-options {:init-ns beanstalk-clj.core}
  )
